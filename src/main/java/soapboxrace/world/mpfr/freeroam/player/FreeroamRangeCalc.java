70cac795e64a9f51f4508562173e963e12e00052
heyitsleo <coderleo42+leocodes@gmail.com>
2018-05-28 09:39:03 -0400

    test range update

--- src/main/java/soapboxrace/world/mpfr/freeroam/player/FreeroamRangeCalc.java
+++ src/main/java/soapboxrace/world/mpfr/freeroam/player/FreeroamRangeCalc.java
@@ -1,8 +1,11 @@
 package soapboxrace.world.mpfr.freeroam.player;
 
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import soapboxrace.world.mpfr.freeroam.FreeroamTalker;
 import soapboxrace.world.mpfr.server.udp.FreeroamAllTalkers;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -17,27 +20,53 @@ public class FreeroamRangeCalc
 
     private static FreeroamVisibleTalkers.PlayerListDifferences setVisibleClosestPlayers(FreeroamTalker freeroamTalker)
     {
-        List<FreeroamTalker> closestList = FreeroamAllTalkers.getFreeroamTalkers()
+        List<FreeroamTalker> talkers = FreeroamAllTalkers.getFreeroamTalkers()
                 .entrySet()
                 .stream()
-                .filter(t -> !t.getValue().equals(freeroamTalker))
-                .filter(t -> t.getValue().isReady())
-                .sorted(Comparator.comparingDouble(t -> {
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+        List<Pair<FreeroamTalker, Double>> distancePairs = talkers.stream()
+                .map(talker -> {
                     int[] self = {
                             freeroamTalker.getXPos(),
                             freeroamTalker.getYPos(),
                     };
                     int[] them = {
-                            t.getValue().getXPos(),
-                            t.getValue().getYPos()
+                            talker.getXPos(),
+                            talker.getYPos()
                     };
-                    return Math.sqrt(Math.pow(them[0] - self[0], 2) + Math.pow(them[1] - self[1], 2));//
-                }))
+                    
+                    return ImmutablePair.of(talker, Math.sqrt(Math.pow(them[0] - self[0], 2) + Math.pow(them[1] - self[1], 2)));
+                })
+                .filter(pair -> pair.getRight() <= 100) // change?
+                .sorted(Comparator.comparingDouble(Pair::getValue))
                 .limit(FreeroamVisibleTalkers.VISIBLE_PLAYERS_LIMIT)
-                .map(Map.Entry::getValue)
                 .collect(Collectors.toList());
-//        MainLogger.logger.debug("closest for [{}] = {}", freeroamTalker, closestList);
+        return freeroamTalker.getVisibleTalkers().setVisibleTalkers(
+                distancePairs.stream().map(Pair::getKey).collect(Collectors.toList())
+        );
         
-        return freeroamTalker.getVisibleTalkers().setVisibleTalkers(closestList);
+//        List<FreeroamTalker> closestList = FreeroamAllTalkers.getFreeroamTalkers()
+//                .entrySet()
+//                .stream()
+//                .filter(t -> !t.getValue().equals(freeroamTalker))
+//                .filter(t -> t.getValue().isReady())
+//                .sorted(Comparator.comparingDouble(t -> {
+//                    int[] self = {
+//                            freeroamTalker.getXPos(),
+//                            freeroamTalker.getYPos(),
+//                    };
+//                    int[] them = {
+//                            t.getValue().getXPos(),
+//                            t.getValue().getYPos()
+//                    };
+//                    return Math.sqrt(Math.pow(them[0] - self[0], 2) + Math.pow(them[1] - self[1], 2));//
+//                }))
+//                .limit(FreeroamVisibleTalkers.VISIBLE_PLAYERS_LIMIT)
+//                .map(Map.Entry::getValue)
+//                .collect(Collectors.toList());
+////        MainLogger.logger.debug("closest for [{}] = {}", freeroamTalker, closestList);
+//        
+//        return freeroamTalker.getVisibleTalkers().setVisibleTalkers(closestList);
     }
 }
