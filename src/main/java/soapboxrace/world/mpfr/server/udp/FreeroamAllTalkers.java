08e9c039a8ff5feb54146c9826659389b54bf4c2
heyitsleo <coderleo42+leocodes@gmail.com>
2018-06-29 06:37:01 -0400

    Testing optimizations

--- src/main/java/soapboxrace/world/mpfr/server/udp/FreeroamAllTalkers.java
+++ src/main/java/soapboxrace/world/mpfr/server/udp/FreeroamAllTalkers.java
@@ -3,21 +3,25 @@ package soapboxrace.world.mpfr.server.udp;
 import java.util.HashMap;
 
 import io.netty.channel.socket.DatagramPacket;
+import soapboxrace.world.mpfr.freeroam.FreeroamLoop;
 import soapboxrace.world.mpfr.freeroam.FreeroamTalker;
 import soapboxrace.world.mpfr.freeroam.FreeroamTalkerCleanup;
 
 public class FreeroamAllTalkers
 {
-
     private static HashMap<Integer, FreeroamTalker> freeroamTalkers = new HashMap<>();
+    private static boolean startedTasks = false;
 
     public static void put(FreeroamTalker freeroamTalker)
     {
         freeroamTalkers.put(freeroamTalker.getPort(), freeroamTalker);
         
-        if (freeroamTalkers.size() == 1)
+        if (freeroamTalkers.size() == 1 && !startedTasks)
         {
             new FreeroamTalkerCleanup();
+            new FreeroamLoop();
+            
+            startedTasks = true;
         }
     }
 
